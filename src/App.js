502a70d705d3072f5a1e3f20dc7f36cbaab7091f
Alexander Voevodin <43845313+windcrack@users.noreply.github.com>
2022-07-05 22:34:49 +0300

    Practice on React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({
+    path: "",
+    files: [],
+  });
+  const [parent, setParent] = useState('')
+  
+  useEffect(()=>{
+    fetch("http://localhost:8000")
+    .then(res => res.json())
+    .then(result => {
+      setParent('')
+      setData(result)
+    }, (error) =>{
+      console.log(error);
+    })
+  }, []);
+
+  const clickHandler = event =>{
+    event.preventDefault();
+    // console.log(event.target.attributes.href.value); 
+    fetch("http://localhost:8000/?path="+event.target.attributes.href.value)
+    .then(res => res.json())
+    .then(result =>{
+      let linkArr = result.path.split('/');
+      linkArr.pop();
+      setParent(linkArr.join('/'));
+      setData(result);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="file-manager">
+      <div className='current-level'>
+        current: {data.path === "" ? "/" : data.path}
+      </div>
+      <div>
+        <a href={parent} onClick={clickHandler}>
+          Level up <span className="material-symbols-outlined">expand_less</span>
         </a>
-      </header>
+      </div>
+      <ul className="folder-list">
+        {data.files.map((item, idx) => {
+
+          if(item.dir){
+            return <li key={idx} className="folder">
+                    <span className='material-icons'>folder</span>
+                    <a href={data.path+'/'+item.name} onClick={clickHandler}>
+                      {item.name.toUpperCase()}
+                    </a>
+                  </li>
+          }else{
+            return <li key={idx} className="file">
+                      <span className="material-symbols-outlined">docs</span>
+                      {item.name}
+                    </li>
+          }
+
+        })}
+      </ul>
     </div>
   );
 }
